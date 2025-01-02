03d395bba722db4e6e06144bf0c9c8997aa53f54
Javier López <lopez.pardo@gmail.com>
2025-01-02 11:28:25 +0100

    add second dependency with same internal dependency

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 const externalDep = require('@yarn-talk/external-dep');
+const externalDep2 = require('@yarn-talk/external-dep2');
 
-console.log(externalDep);
+console.log({externalDep, externalDep2});
