d12563cb9655b524bbf62e8663e4efd558ab7a25
Javier López <lopez.pardo@gmail.com>
2025-01-02 11:46:40 +0100

    add new dependency with fixed version

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 const externalDep = require('@yarn-talk/external-dep');
 const externalDep2 = require('@yarn-talk/external-dep2');
+const externalDepFixed = require('@yarn-talk/external-dep-fixed');
 
-console.log({externalDep, externalDep2});
+console.log({
+  externalDep: externalDep.internalDep.version,
+  externalDep2: externalDep2.internalDep.version,
+  externalDepFixed: externalDepFixed.internalDep.version
+});
